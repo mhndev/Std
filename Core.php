b58ee6f3fd4bd1117cff69939d5005dcfa566fc1
Payam Naderi <naderi.payam@gmail.com>
2014-11-22 15:57:52 +0330

    Initialize First Commit

---
+++ Core.php
@@ -0,0 +1,33 @@
+<?php
+namespace Poirot\Core {
+    /**
+     * Merge two arrays together.
+     *
+     * If an integer key exists in both arrays, the value from the second array
+     * will be appended the the first array. If both values are arrays, they
+     * are merged together, else the value of the second array overwrites the
+     * one of the first array.
+     *
+     * @param  array $a
+     * @param  array $b
+     * @return array
+     */
+    function array_merge(array $a, array $b)
+    {
+        foreach ($b as $key => $value) {
+            if (array_key_exists($key, $a)) {
+                if (is_int($key)) {
+                    $a[] = $value;
+                } elseif (is_array($value) && is_array($a[$key])) {
+                    $a[$key] = array_merge($a[$key], $value);
+                } else {
+                    $a[$key] = $value;
+                }
+            } else {
+                $a[$key] = $value;
+            }
+        }
+
+        return $a;
+    }
+}
