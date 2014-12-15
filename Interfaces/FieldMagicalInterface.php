23d6ef1ce38c0ec6b4ee1389401d64ec90628679
Payam Naderi <naderi.payam@gmail.com>
2014-12-15 14:46:57 +0330

    implement Magical Field Interface

---
+++ Interfaces/FieldMagicalInterface.php
@@ -0,0 +1,30 @@
+<?php
+namespace Poirot\Core\Interfaces;
+
+interface FieldMagicalInterface
+{
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
+    public function __set($key, $value);
+
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    public function __get($key);
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function __isset($key);
+
+    /**
+     * @param string $key
+     * @return void
+     */
+    public function __unset($key);
+}
