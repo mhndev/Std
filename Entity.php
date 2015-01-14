a52ebc95244ea5ac1ce873360f0a69e8db7c8b33
Payam Naderi <naderi.payam@gmail.com>
2015-01-14 15:02:59 +0330

    code cleanup

--- Entity.php
+++ Entity.php
@@ -187,6 +187,14 @@ class Entity implements Interfaces\EntityInterface
     /**
      * Output Conveyor Props. as desired manipulated data struct.
      *
+     * ! Be Aware of the situation for classes that extend Entity
+     *   and maybe have stored original properties in the other way
+     *   instead of $this->properties in exp. for session storage,
+     *   so i prefer use:
+     *   [code]
+     *      return $this->getAs(new Entity($this));
+     *   [/code]
+     *
      * @return mixed
      */
     public function borrow()
