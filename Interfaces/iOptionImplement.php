c18de9d31709f0e6310a9600f9d50e36a613c9ac
Payam Naderi <naderi.payam@gmail.com>
2015-03-14 12:49:42 +0330

    Implement OpenOptions As Trait

---
+++ Interfaces/iOptionImplement.php
@@ -0,0 +1,62 @@
+<?php
+namespace Poirot\Core\Interfaces;
+
+use Poirot\Core\AbstractOptions\PropsObject;
+
+interface iOptionImplement extends iMagicalFields
+{
+    /**
+     * Set Options
+     *
+     * @param array|iPoirotOptions $options
+     *
+     * @return $this
+     */
+    function from($options);
+
+    /**
+     * Set Options From Array
+     *
+     * * only get associated array
+     *
+     * @param array $options Options Array
+     *
+     * @throws \Exception
+     * @return $this
+     */
+    function fromArray(array $options);
+
+    /**
+     * Set Options From Same Option Object
+     *
+     * note: it will take an option object instance of $this
+     *       OpenOptions only take OpenOptions as argument
+     *
+     * - also you can check for private and write_only
+     *   methods inside Options Object to get fully coincident copy
+     *   of Options Class Object
+     *
+     * @param iOptionImplement $options Options Object
+     *
+     * @throws \Exception
+     * @return $this
+     */
+    function fromOption(iOptionImplement $options);
+    // PHP rise Deceleration Fatal Error even on options
+    // that is extend iPoirotOptions interface
+
+
+    /**
+     * Get Properties as array
+     *
+     * @return array
+     */
+    function toArray();
+
+    /**
+     * Get Options Properties Information
+     *
+     * @return PropsObject
+     */
+    function props();
+}
--- Interfaces/iPoirotOptions.php
+++ Interfaces/iPoirotOptions.php
@@ -1,69 +1,12 @@
 <?php
 namespace Poirot\Core\Interfaces;
 
-use Poirot\Core\AbstractOptions\PropsObject;
-
-interface iPoirotOptions extends iMagicalFields
+interface iPoirotOptions extends iOptionImplement
 {
     /**
      * Construct
      *
-     * @param array|iPoirotOptions $options Options
+     * @param array|iOptionImplement $options Options
      */
     function __construct($options = null);
-
-    /**
-     * Set Options
-     *
-     * @param array|iPoirotOptions $options
-     *
-     * @return $this
-     */
-    function from($options);
-
-    /**
-     * Set Options From Array
-     *
-     * * only get associated array
-     *
-     * @param array $options Options Array
-     *
-     * @throws \Exception
-     * @return $this
-     */
-    function fromArray(array $options);
-
-    /**
-     * Set Options From Same Option Object
-     *
-     * note: it will take an option object instance of $this
-     *       OpenOptions only take OpenOptions as argument
-     *
-     * - also you can check for private and write_only
-     *   methods inside Options Object to get fully coincident copy
-     *   of Options Class Object
-     *
-     * @param iPoirotOptions $options Options Object
-     *
-     * @throws \Exception
-     * @return $this
-     */
-    function fromOption(iPoirotOptions $options);
-    // PHP rise Deceleration Fatal Error even on options
-    // that is extend iPoirotOptions interface
-
-
-    /**
-     * Get Properties as array
-     *
-     * @return array
-     */
-    function toArray();
-
-    /**
-     * Get Options Properties Information
-     *
-     * @return PropsObject
-     */
-    function props();
 }
