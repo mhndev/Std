f7fed5b44a04e3cdb5f32d1828a1234c69a0e92c
Payam Naderi <naderi.payam@gmail.com>
2015-09-19 20:05:29 +0430

    implement invokable interface.

---
+++ Interfaces/iInvokable.php
@@ -0,0 +1,7 @@
+<?php
+namespace Poirot\Core\Interfaces;
+
+interface iInvokable 
+{
+    function __invoke($argument = null);
+} 
---
+++ Interfaces/iInvokableCallback.php
@@ -0,0 +1,26 @@
+<?php
+namespace Poirot\Core\Interfaces;
+
+interface iInvokableCallback extends iInvokable
+{
+    /**
+     * Set Callable Closure For __invoke
+     *
+     * - callable can have one optional argument. exp.func(arg)
+     * - callable bind to this class as a closure, so with
+     *   $this on function you can access methods/vars on this
+     *   class
+     *
+     * @param callable $callable
+     *
+     * @return $this
+     */
+    function setCallable(callable $callable);
+
+    /**
+     * Get Callable
+     *
+     * @return callable
+     */
+    function getCallable();
+}
