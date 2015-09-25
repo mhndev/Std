bdf97e4c7730e084200884932676d428d613fab9
Payam Naderi <naderi.payam@gmail.com>
2015-09-25 21:33:34 +0330

    implement open call as trait

---
+++ OpenCall.php
@@ -0,0 +1,10 @@
+<?php
+namespace Poirot\Core;
+
+use Poirot\Core\Traits\OpenCallTrait;
+
+class OpenCall
+{
+    use OpenCallTrait;
+}
+ 
--- Traits/OpenCallTrait.php
+++ Traits/OpenCallTrait.php
@@ -1,16 +1,37 @@
 <?php
 namespace Poirot\Core\Traits;
 
-trait OpenCall
+trait OpenCallTrait
 {
     private $_t__methods = [];
+    private $_t__bindto;
+
+    function bindTo($class)
+    {
+        if (!is_object($class))
+            throw new \Exception('Class is not object.');
+
+        $this->_t__bindto = $class;
+    }
+
+    function getBindTo()
+    {
+        if (!$this->_t__bindto)
+            $this->bindTo($this);
+
+        return $this->_t__bindto;
+    }
 
     function addMethod($methodName, $methodCallable)
     {
         if (!is_callable($methodCallable))
             throw new \InvalidArgumentException('Second param must be callable');
 
-        $this->_t__methods[$methodName] = \Closure::bind($methodCallable, $this, get_class());
+        $this->_t__methods[$methodName] = \Closure::bind(
+            $methodCallable
+            , $this->getBindTo()
+            , get_class($this->getBindTo())
+        );
     }
 
     function __call($methodName, array $args)
