e58e92a2cdcbf8e7e130f5cd565052ed355b1ece
Payam Naderi <naderi.payam@gmail.com>
2015-10-06 10:16:53 +0330

    code cleanup

--- Traits/OpenCallTrait.php
+++ Traits/OpenCallTrait.php
@@ -4,41 +4,90 @@ namespace Poirot\Core\Traits;
 trait OpenCallTrait
 {
     private $_t__methods = [];
-    private $_t__bindto;
+    private $_t__bindTo;
 
+    /**
+     * Bind Current Methods to Given Object
+     *
+     * @param $class
+     *
+     * @throws \Exception
+     * @return $this
+     */
     function bindTo($class)
     {
         if (!is_object($class))
-            throw new \Exception('Class is not object.');
+            throw new \Exception(sprintf(
+                'Given class must be an object (%s) given.'
+                , \Poirot\Core\flatten($class)
+            ));
 
-        $this->_t__bindto = $class;
+        $this->_t__bindTo = $class;
+
+        return $this;
     }
 
+    /**
+     * Get Given Bind Object
+     *
+     * @return object
+     */
     function getBindTo()
     {
-        if (!$this->_t__bindto)
+        if (!$this->_t__bindTo)
             $this->bindTo($this);
 
-        return $this->_t__bindto;
+        return $this->_t__bindTo;
     }
 
-    function addMethod($methodName, $methodCallable)
+    /**
+     * Attach Method To This Class
+     *
+     * @param string   $methodName
+     * @param \Closure $methodCallable
+     *
+     * @return $this
+     */
+    function addMethod($methodName, \Closure $methodCallable)
     {
-        if (!is_callable($methodCallable))
-            throw new \InvalidArgumentException('Second param must be callable');
+        $this->_t__methods[$methodName] = $methodCallable;
 
-        $this->_t__methods[$methodName] = \Closure::bind(
+        return $this;
+    }
+
+    /**
+     * Has Method Name Exists?
+     *
+     * @param string $methodName
+     *
+     * @return bool
+     */
+    function hasMethod($methodName)
+    {
+        return isset($this->_t__methods[$methodName]);
+    }
+
+    /**
+     * Proxy Call To Registered Methods
+     *
+     * @param $methodName
+     * @param array $args
+     *
+     * @return mixed
+     */
+    function __call($methodName, array $args)
+    {
+        if (!$this->hasMethod($methodName))
+            throw new \RunTimeException('There is no method with the given name to call');
+
+        $methodCallable = $this->_t__methods[$methodName];
+        ## bind it into latest bind object
+        $methodCallable = \Closure::bind(
             $methodCallable
             , $this->getBindTo()
             , get_class($this->getBindTo())
         );
-    }
 
-    function __call($methodName, array $args)
-    {
-        if (isset($this->_t__methods[$methodName]))
-            return call_user_func_array($this->_t__methods[$methodName], $args);
-
-        throw new \RunTimeException('There is no method with the given name to call');
+        return call_user_func_array($methodCallable, $args);
     }
 }
