1fd354c2b744c2c5973e9e815008be61ec4b0620
Payam Naderi <naderi.payam@gmail.com>
2015-10-20 17:17:39 +0330

    improve error stack handler for chaining exception handlers

--- ErrorStack.php
+++ ErrorStack.php
@@ -4,6 +4,7 @@ namespace Poirot\Core;
 use ErrorException;
 
 /*
+ * 
 ErrorStack::handleBegin(function($errno, $errstr = '', $errfile = '', $errline = 0) {
     ## this will print error string to output
     var_dump($errstr);
@@ -21,6 +22,28 @@ echo $not_defined_variable;
 $error = ErrorStack::handleDone();
 if ($error)
     throw $error;
+
+// == Chain Exception Handlers ==========================================================
+
+ErrorStack::handleException(function ($e) {
+    ## then accrued exception rise to php default
+    throw $e;
+});
+
+ErrorStack::handleException(function ($e) {
+    echo 'next we see this<br/>';
+
+    ## pass to next error handler by throwing exception
+    throw $e;
+});
+
+ErrorStack::handleException(function ($e) {
+    echo 'this error will appear once.<br/>';
+
+    ## pass to next error handler by throwing exception
+    throw $e;
+});
+
 */
 
 class ErrorStack
@@ -63,10 +86,33 @@ class ErrorStack
      *
      * - handleBegin([$object, 'method'])
      *
+     * @param callable|null $callable
+     */
+    static function handleException(callable $callable = null)
+    {
+        $self = get_called_class();
+        set_exception_handler("{$self}::_handleError");
+
+        self::$_STACK[] = [
+            'error_level' => null,
+            'callable'    => $callable,
+            'has_error'   => null,
+            '__handle__'  => 'exception',
+        ];
+    }
+
+    /**
+     * Used for defining your own way of handling errors during runtime,
+     * for example in applications in which you need to do cleanup of -
+     * data/files when a critical error happens, or when you need to -
+     * trigger an error under certain conditions
+     *
+     * - handleError([$object, 'method'])
+     *
      * @param int $errorLevel
      * @param callable|null $callable
      */
-    static function handleBegin($errorLevel = self::ERR_DEF_SEVERITY, callable $callable = null)
+    static function handleError($errorLevel = self::ERR_DEF_SEVERITY, callable $callable = null)
     {
         if (is_callable($errorLevel)) {
             $callable   = $errorLevel;
@@ -80,11 +126,28 @@ class ErrorStack
             'error_level' => $errorLevel,
             'callable'    => $callable,
             'has_error'   => null,
+            '__handle__'  => 'error',
         ];
     }
 
     /**
+     * Get Current Accured Error If Has
+     *
+     * @return null|\Exception|\ErrorException
+     */
+    static function getAccuredErr()
+    {
+        if (!self::hasHandling())
+            return null;
+
+        $stack = self::$_STACK[self::getLevel()-1];
+        return $stack['has_error'];
+    }
+
+    /**
      * Stopping the error handler
+     *
+     * - return last error if it exists
      *
      * @return null|ErrorException
      */
@@ -96,11 +159,12 @@ class ErrorStack
             ## there is no error
             return $return;
 
-        restore_error_handler();
-
         $stack = array_pop(self::$_STACK);
         if ($stack['has_error'])
             $return = $stack['has_error'];
+
+        # restore error handler
+        (($stack['__handle__']) == 'error') ? restore_error_handler() : restore_exception_handler();
 
         return $return;
     }
@@ -112,18 +176,54 @@ class ErrorStack
     static function clean()
     {
         restore_error_handler();
+        restore_exception_handler();
 
         self::$_STACK = [];
     }
 
+    /**
+     * Handle Both Exception And Errors That Happen Within
+     * handleBegin/handleDone
+     *
+     * @private
+     */
     static function _handleError($errno, $errstr = '', $errfile = '', $errline = 0)
     {
         $stack = & self::$_STACK[self::getLevel()-1];
-        $stack['has_error'] = new ErrorException($errstr, 0, $errno, $errfile, $errline);
+
+        if ($errno instanceof \Exception) {
+            ## catch exception within handle begin
+            $stack['has_error']  = $errno;
+            $stack['__handle__'] = 'exception';
+        }
+        else {
+            $stack['has_error']  = new ErrorException($errstr, 0, $errno, $errfile, $errline);
+            $stack['__handle__'] = 'error';
+        }
+
+
+        // ...
 
         if (isset($stack['callable']))
-            ## call user error handler callable
-            call_user_func($stack['callable'], $errno, $errstr, $errfile, $errline);
+            try {
+                ## call user error handler callable
+                ## exception will passed as errno on exception catch
+                call_user_func($stack['callable'], $errno, $errstr, $errfile, $errline);
+            }
+            catch (\Exception $e) {
+                ## during handling an error if any exception happen it must handle with parent handler
+                self::handleDone();
+
+                while (self::hasHandling()) {
+                    $stack = & self::$_STACK[self::getLevel()-1];
+                    if ($stack['__handle__'] == 'exception') {
+                        self::_handleError($e);
+                        break;
+                    }
+
+                    self::handleDone();
+                }
+            }
     }
 
     /**
