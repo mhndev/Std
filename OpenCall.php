f3932f916c6a273b0965712ab0dd02a40ba422fb
Payam Naderi <naderi.payam@gmail.com>
2015-12-06 11:04:29 +0330

    implement opencall constructor

--- OpenCall.php
+++ OpenCall.php
@@ -6,5 +6,27 @@ use Poirot\Core\Traits\OpenCallTrait;
 class OpenCall
 {
     use OpenCallTrait;
+
+    /**
+     * Construct
+     *
+     * @param null  $bindTo
+     * @param array $methods ['method_name' => \Closure]
+     *
+     * @throws \Exception
+     */
+    function __construct($bindTo = null, array $methods = [])
+    {
+        if (!is_object($bindTo) && $bindTo !== null)
+            throw new \InvalidArgumentException(sprintf(
+                'Invalid argument BindTo for OpenCall Construct, must given Object instead "%s" given.'
+                , \Poirot\Core\flatten($bindTo)
+            ));
+
+        if ($bindTo)
+            $this->bindTo($bindTo);
+
+        foreach($methods as $m => $f)
+            $this->addMethod($m, $f);
+    }
 }
- 
