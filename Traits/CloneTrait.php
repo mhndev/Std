990c41b8656f0f69cfe3b7dd42c02197848d00ce
Payam Naderi <naderi.payam@gmail.com>
2015-12-21 09:49:08 +0330

    improve clone method

--- Traits/CloneTrait.php
+++ Traits/CloneTrait.php
@@ -5,7 +5,20 @@ trait CloneTrait
 {
     function __clone()
     {
-        foreach(get_object_vars($this) as $name => $val)
-            (!is_object($val)) ?: $this->{$name} = clone $this->{$name};
+        $_f__clone_array = function($arr) use (&$_f__clone_array) {
+            foreach ($arr as &$v) {
+                if (is_array($v))
+                    $_f__clone_array($v);
+                elseif (is_object($v))
+                    $v = clone $v;
+            }
+        };
+
+        foreach($this as &$val) {
+            if (is_array($val))
+                $_f__clone_array($val);
+            elseif (is_object($val))
+                $val = clone $val;
+        }
     }
 }
