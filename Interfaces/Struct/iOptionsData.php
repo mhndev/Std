03a51bd61e38cfd20e71e54c7b7c8881c03343b3
Payam Naderi <naderi.payam@gmail.com>
2016-04-06 15:41:19 +0430

    code cleanup

--- Interfaces/Struct/iOptionsData.php
+++ Interfaces/Struct/iOptionsData.php
@@ -1,10 +1,16 @@
 <?php
 namespace Poirot\Std\Interfaces\Struct;
 
+/**
+ * Options Is DataStruct Can Be Check Against Fulfillment
+ *
+ */
 interface iOptionsData extends iDataStruct
 {
     /**
      * Is Required Property Full Filled?
+     *
+     * - with no property it will check for whole properties
      *
      * @param null|string $property_key
      *
